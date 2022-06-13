d204062869032fdf943a4228f8be2fbf144730c5
aditya <88678444+lucifer2661@users.noreply.github.com>
2022-06-13 19:18:12 +0530

    Add files via upload

---
+++ New Text Document.c
@@ -0,0 +1,15 @@
+int main() {
+
+  int appleQuantity=23;
+  double applePrice=1.49;
+  double appleReview=82.5;
+  int appleReviewDisplay=82;
+  const char appleLocation='f';
+  appleReview= (int)appleReviewDisplay;
+
+  
+
+// Put all your code above this and if you declare your variables using the given names and types there is no need to change any of the code below.
+printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
